a00f2708b5d6828aa6c149f81153a23bfdb74d65
psnmurthy9 <psnmurthy140793@gmail.com>
2025-02-15 17:40:19 +0530

    Base URL changed  commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(helmet({
 }))
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://vruksha.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
