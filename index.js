ced5ed78b5642ddfd4c5fcda3bfea897af51fa7b
psnmurthy9 <psnmurthy140793@gmail.com>
2025-02-15 17:32:59 +0530

    Base URL changed  commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express()
 
 app.use(cors({
     credentials : true,
-    origin : 'http://localhost:5173'
+    origin : 'https://vruksha.vercel.app/'
 }))
 app.use(express.json())
 app.use(cookieParser())
